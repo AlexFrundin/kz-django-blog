0ad61ea353459824a706116e5049256ef08faad4
AlexFrundin <aleksey.frundin@gmail.com>
2021-02-01 13:14:10 +0200

    bug-fix

--- blog/core/v2/urls.py
+++ blog/core/v2/urls.py
@@ -1,8 +1,8 @@
-from django.urls import path
+# from django.urls import path
 
-from . import views
+# from . import views
 
-urlpatterns = [
-    path('<str:name>', views.begin, name='get_name'),
-    path('register/<str:name>', views.write,  name='register'),
-]
+# urlpatterns = [
+#     path('<str:name>', views.begin, name='get_name'),
+#     path('register/<str:name>', views.write,  name='register'),
+# ]
--- blog/core/v2/views.py
+++ blog/core/v2/views.py
@@ -1,31 +1,31 @@
-from django.http import JsonResponse
+# from django.http import JsonResponse
 
 
-def begin(request, name):
-    return JsonResponse({'name': name*3})
+# def begin(request, name):
+#     return JsonResponse({'name': name*3})
 
 
-def write(request, name):
-    return JsonResponse({'name': name, 'version': 'v2'})
+# def write(request, name):
+#     return JsonResponse({'name': name, 'version': 'v2'})
 
 
-class WriteView(TemplateView):
-    template_name = 'core/register.html'
+# class WriteView(TemplateView):
+#     template_name = 'core/register.html'
 
-    def get_context_data(self, name, *args, **kwargs):
-        context = super().get_context_data(*args, **kwargs)
-        context['name'] = name
-        return context
+#     def get_context_data(self, name, *args, **kwargs):
+#         context = super().get_context_data(*args, **kwargs)
+#         context['name'] = name
+#         return context
 
-    def get(self, request, name):
-        return self.render_to_response(self.get_context_data(name))
+#     def get(self, request, name):
+#         return self.render_to_response(self.get_context_data(name))
 
-    def post(self, request, name):
-        data = request.POST
-        if data['password'] == data['password2'] and name not in data['login']:
-            messages.success(request, message='Ура! Наша дружба стала крепче!')
-            return redirect('movie_all')
-        messages.add_message(request,
-                             message='Вы бяка! Я с вами не дружу!!!',
-                             level=messages.WARNING)
-        return self.get(request, name)
+#     def post(self, request, name):
+#         data = request.POST
+#         if data['password'] == data['password2'] and name not in data['login']:
+#             messages.success(request, message='Ура! Наша дружба стала крепче!')
+#             return redirect('movie_all')
+#         messages.add_message(request,
+#                              message='Вы бяка! Я с вами не дружу!!!',
+#                              level=messages.WARNING)
+#         return self.get(request, name)
